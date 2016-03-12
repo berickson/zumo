f032e894de5d990202ec62217c8bb976a43b8bba
Brian Erickson <berickson@gmail.com>
2016-03-11 23:37:29 -0800

    ready for match

--- around_the_box/around_the_box.ino
+++ around_the_box/around_the_box.ino
@@ -20,13 +20,14 @@ Zumo32U4Motors motors;
 Zumo32U4Encoders encoders;
 Zumo32U4ProximitySensors proximity_sensors;
 
+struct Location {double x; double y;} location = {0.,0.};
+
 L3G gyro;
 
 int start_millis;
-void setup()
-{
+
+void setup() {
   turnSensorSetup();
-  delay(500);
   turnSensorReset();
   proximity_sensors.initThreeSensors();
   start_millis = millis();
@@ -41,24 +42,27 @@ bool every_n_ms(unsigned long last_loop_ms, unsigned long loop_ms, unsigned long
 
 // returns theta in [-180,180)
 double standardized_degrees(double theta) {
-  return fmod((theta + 180 + 360), 360.) - 180.;
+  return fmod((theta + 180 + 3600), 360.) - 180.;
 }
 
 void show_stats_on_lcd() {
     lcd.clear();
     lcd.print(standardized_degrees(turn_degrees));
     lcd.gotoXY(0,1);
-    lcd.print(proximity_sensors.countsRightWithLeftLeds());
+    //lcd.print(proximity_sensors.countsRightWithLeftLeds());
+    //lcd.print(",");
+    //lcd.print(proximity_sensors.countsRightWithRightLeds());
+    lcd.print((int)location.x);
     lcd.print(",");
-    lcd.print(proximity_sensors.countsRightWithRightLeds());
+    lcd.print((int)location.y);
 }
 
 
-void go_forward(double desired_degrees) {
+void go_forward(double desired_degrees, double speed = max_speed) {
   double heading_error = standardized_degrees(desired_degrees - turn_degrees);
 
-  int left_speed = constrain(max_speed - 20 * heading_error,-max_speed,max_speed);
-  int right_speed = constrain(max_speed + 20 * heading_error,-max_speed,max_speed);
+  int left_speed = constrain(speed - 20 * heading_error,-max_speed,max_speed);
+  int right_speed = constrain(speed + 20 * heading_error,-max_speed,max_speed);
   
   
   motors.setSpeeds(left_speed, right_speed);
@@ -76,12 +80,42 @@ bool turn_to_angle(float desired_degrees) {
   // -maxSpeed and maxSpeed.
   motor_speed= constrain(motor_speed, -max_speed, max_speed);
 
-  motors.setSpeeds(-motor_speed, motor_speed);
-  if(abs(turn_error)<0.5 && turnRate < 10) {
+  if(abs(turn_error)<1.0 && abs(turnRate) < 10) {
+    motors.setSpeeds(-0,0);
     return true;
   }
+
+  // enforce minimum motor speed
+  if(abs(motor_speed) < 50 && abs(turnRate) < 10) {
+    if(motor_speed < 0) 
+      motor_speed = -50;
+    else
+      motor_speed = 50;
+  }
+  motors.setSpeeds(-motor_speed, motor_speed);
   return false;
   
+}
+
+double heading_degrees_to(double x, double y) {
+  double dx = x - location.x;
+  double dy = y - location.y;
+  return atan2(dy, dx) * 180./PI;
+}
+
+bool move_to(double x, double y) {
+  double dx = x - location.x;
+  double dy = y - location.y;
+  double heading_sp = atan2(dy, dx) * 180./PI;
+  double remaining = sqrt(dx*dx+dy*dy);
+  if(remaining < 100) {
+    motors.setSpeeds(0,0);
+    return true;
+  }
+ 
+  double speed = remaining*0.5;
+  go_forward(heading_sp, speed);
+  return false;
 }
 
 void loop()
@@ -98,9 +132,15 @@ void loop()
 
   // update all sensors
   turnSensorUpdate();
-  int16_t counts_left = encoders.getCountsLeft();
-  int16_t counts_right = encoders.getCountsRight();
   proximity_sensors.read();
+
+
+  // update robot location 
+  {
+    double forward_distance = encoders.getCountsAndResetLeft() + encoders.getCountsAndResetRight();
+    location.x += forward_distance * cos(turn_degrees * PI/180.);
+    location.y += forward_distance * sin(turn_degrees * PI/180.);
+  }
 
   
   if(every_100_ms)  {
@@ -126,7 +166,7 @@ void loop()
     case 3:
     case 5:
     case 7:
-    case 9:
+ //   case 9:
       go_forward(go_angle);
       if (last_box_to_right && !box_to_right) {
         go_angle -= 90;
@@ -142,11 +182,28 @@ void loop()
     case 8:
       if (turn_to_angle(go_angle)) {
         ++current_step;
-        lcd.clear();
-        lcd.print((String)"step "+current_step);
       }
       break;
+    case 9:
+      current_step++;
+      break;
     case 10:
+      if(turn_to_angle(heading_degrees_to(0,0))) {
+        current_step++;
+      }
+      break;
+
+    case 11:
+      if (move_to(0,0)) {
+        current_step++;
+      }
+      break;
+    case 12:
+      if (turn_to_angle(0.0)) {
+        current_step++;
+      }
+      break;
+    case 13:
       motors.setSpeeds(0,0);
       lcd.clear();
       lcd.print("Done");
@@ -154,7 +211,7 @@ void loop()
       lcd.print("a replay");
       current_step++;
       break;
-    case 11:
+    case 14:
       if(buttonA.getSingleDebouncedRelease()) {
         current_step = 0;
       }
